c1559d470e33f63c0efea523174669af29ed1ae8
James Conerly <jconerly@cpan.org>
2010-12-15 18:59:35 +0000

    Import of JCONERLY/CDN-Edgecast-Client-0.01.00 from CPAN.
    gitpan-cpan-distribution: CDN-Edgecast-Client
    gitpan-cpan-version:      0.01.00
    gitpan-cpan-path:         JCONERLY/CDN-Edgecast-Client-0.01.00.tar.gz
    gitpan-cpan-author:       JCONERLY
    gitpan-cpan-maturity:     released

--- lib/CDN/Edgecast/Client/auto/Reporting/Interface/EdgeCastWebServices/EdgeCastWebServicesSoap.pm
+++ lib/CDN/Edgecast/Client/auto/Reporting/Interface/EdgeCastWebServices/EdgeCastWebServicesSoap.pm
@@ -195,6 +195,81 @@ sub CacheStatusStatsGet {
 }
 
 
+sub TrafficUsageGet {
+    my ($self, $body, $header) = @_;
+    die "TrafficUsageGet must be called as object method (\$self is <$self>)" if not blessed($self);
+    return $self->SUPER::call({
+        operation => 'TrafficUsageGet',
+        soap_action => 'EC:WebServices/TrafficUsageGet',
+        style => 'document',
+        body => {
+            
+
+           'use'            => 'literal',
+            namespace       => 'http://schemas.xmlsoap.org/wsdl/soap/',
+            encodingStyle   => '',
+            parts           =>  [qw( CDN::Edgecast::Client::auto::Reporting::Element::TrafficUsageGet )],
+        },
+        header => {
+            
+        },
+        headerfault => {
+            
+        }
+    }, $body, $header);
+}
+
+
+sub StorageUsageGetMax {
+    my ($self, $body, $header) = @_;
+    die "StorageUsageGetMax must be called as object method (\$self is <$self>)" if not blessed($self);
+    return $self->SUPER::call({
+        operation => 'StorageUsageGetMax',
+        soap_action => 'EC:WebServices/StorageUsageGetMax',
+        style => 'document',
+        body => {
+            
+
+           'use'            => 'literal',
+            namespace       => 'http://schemas.xmlsoap.org/wsdl/soap/',
+            encodingStyle   => '',
+            parts           =>  [qw( CDN::Edgecast::Client::auto::Reporting::Element::StorageUsageGetMax )],
+        },
+        header => {
+            
+        },
+        headerfault => {
+            
+        }
+    }, $body, $header);
+}
+
+
+sub StorageUsageGetLatest {
+    my ($self, $body, $header) = @_;
+    die "StorageUsageGetLatest must be called as object method (\$self is <$self>)" if not blessed($self);
+    return $self->SUPER::call({
+        operation => 'StorageUsageGetLatest',
+        soap_action => 'EC:WebServices/StorageUsageGetLatest',
+        style => 'document',
+        body => {
+            
+
+           'use'            => 'literal',
+            namespace       => 'http://schemas.xmlsoap.org/wsdl/soap/',
+            encodingStyle   => '',
+            parts           =>  [qw( CDN::Edgecast::Client::auto::Reporting::Element::StorageUsageGetLatest )],
+        },
+        header => {
+            
+        },
+        headerfault => {
+            
+        }
+    }, $body, $header);
+}
+
+
 
 
 1;
@@ -226,6 +301,9 @@ version 0.01.00
  $response = $interface->CompleteDownloadsGet();
  $response = $interface->CnameStatsGet();
  $response = $interface->CacheStatusStatsGet();
+ $response = $interface->TrafficUsageGet();
+ $response = $interface->StorageUsageGetMax();
+ $response = $interface->StorageUsageGetLatest();
 
 
 
@@ -274,7 +352,7 @@ hash refs. See the respective class' documentation for additional information.
 
 =head3 CustomerBytesTransferredGet
 
-This method call will get the data transferred (bytes) between begin and end dates for a customer id. <br>Begin and end dates are inclusive and data is granular to the 5-minute interval.<br>Partners may retrieve the information for any of their own customers. Customers may retrieve the information for themselves.<br><br>
+This method will get the data transferred (bytes) between begin and end dates for a customer. <br>Begin and end dates are inclusive and data is granular to the 5-minute interval.<br>Partners may retrieve the information for any of their own customers. Customers may retrieve the information for themselves.<br><br>
 
 Returns a L<CDN::Edgecast::Client::auto::Reporting::Element::CustomerBytesTransferredGetResponse|CDN::Edgecast::Client::auto::Reporting::Element::CustomerBytesTransferredGetResponse> object.
 
@@ -288,7 +366,7 @@ Returns a L<CDN::Edgecast::Client::auto::Reporting::Element::CustomerBytesTransf
 
 =head3 CustomerBytesTransferredByMediaTypeGet
 
-This method call will get the data transferred (bytes) between begin and end dates for a customer id, for a specific media type. <br>Begin and end dates are inclusive and data is granular to the 5-minute interval.<br>Media Type: 1=Windows, 2=Flash, 3=HTTP Large Object, 8=HTTP Small Object. <br>Partners may retrieve the information for any of their own customers. Customers may retrieve the information for themselves.<br><br>
+This method will get the data transferred (bytes) between begin and end dates for a customer, for a specific media type. <br>Begin and end dates are inclusive and data is granular to the 5-minute interval.<br>Media Type: 1=Windows, 2=Flash, 3=HTTP Large Object (includes SSL traffic), 8=HTTP Small Object (includes SSL traffic), 14 = ADN (includes SSL traffic) . <br>Partners may retrieve the information for any of their own customers. Customers may retrieve the information for themselves.<br><br>
 
 Returns a L<CDN::Edgecast::Client::auto::Reporting::Element::CustomerBytesTransferredByMediaTypeGetResponse|CDN::Edgecast::Client::auto::Reporting::Element::CustomerBytesTransferredByMediaTypeGetResponse> object.
 
@@ -303,7 +381,7 @@ Returns a L<CDN::Edgecast::Client::auto::Reporting::Element::CustomerBytesTransf
 
 =head3 FileStatsGet
 
-FOR CUSTOMERS WITH ADVANCED REPORTS. <br>This method call will get data transferred (bytes), hits, daily uniques, and average duration information for the top 250 files. For HTTP, files included in this report are those greater than 1 MB. For streaming, all files are included. <br>Begin and end dates (inclusive) must be in UTC/GMT and data is granular to the day. So if you want to retrieve data for 2009-01-01, begin date would be '2009-01-01T00:00:00-00:00' and end date would be '2009-01-01T23:59:00-00:00'.<br>Media Type: 1=Windows, 2=Flash, 3=HTTP Large Object. <br>Partners may retrieve the information for any of their own customers. Customers may retrieve the information for themselves.<br><br>
+FOR CUSTOMERS WITH ADVANCED REPORTS. <br>This method will get data transferred (bytes), hits, daily uniques, and average duration information for the top 250 files. For HTTP, files included in this report are those greater than 1 MB. For streaming, all files are included. <br>Begin and end dates (inclusive) must be in UTC/GMT and data is granular to the day. So if you want to retrieve data for 2009-01-01, begin date would be '2009-01-01T00:00:00-00:00' and end date would be '2009-01-01T23:59:00-00:00'.<br>Media Type: 1=Windows, 2=Flash, 3=HTTP Large Object. <br>Partners may retrieve the information for any of their own customers. Customers may retrieve the information for themselves.<br><br>
 
 Returns a L<CDN::Edgecast::Client::auto::Reporting::Element::FileStatsGetResponse|CDN::Edgecast::Client::auto::Reporting::Element::FileStatsGetResponse> object.
 
@@ -318,7 +396,7 @@ Returns a L<CDN::Edgecast::Client::auto::Reporting::Element::FileStatsGetRespons
 
 =head3 DirStatsGet
 
-FOR CUSTOMERS WITH ADVANCED REPORTS. <br>This method call will get data transferred (bytes), hits, and average duration information for the top 250 directories. The data is for files, of the media type specified, that sit in the directory at the first level. For HTTP, files included in this report are those greater than 1 MB. For streaming, all files are included. <br>Begin and end dates (inclusive) must be in UTC/GMT and data is granular to the day. So if you want to retrieve data for 2009-01-01, begin date would be '2009-01-01T00:00:00-00:00' and end date would be '2009-01-01T23:59:00-00:00'.<br>Media Type: 1=Windows, 2=Flash, 3=HTTP Large Object. <br>Partners may retrieve the information for any of their own customers. Customers may retrieve the information for themselves.<br><br>
+FOR CUSTOMERS WITH ADVANCED REPORTS. <br>This method will get data transferred (bytes), hits, and average duration information for the top 250 directories. The data is for files, of the media type specified, that sit in the directory at the first level. For HTTP, files included in this report are those greater than 1 MB. For streaming, all files are included. <br>Begin and end dates (inclusive) must be in UTC/GMT and data is granular to the day. So if you want to retrieve data for 2009-01-01, begin date would be '2009-01-01T00:00:00-00:00' and end date would be '2009-01-01T23:59:00-00:00'.<br>Media Type: 1=Windows, 2=Flash, 3=HTTP Large Object. <br>Partners may retrieve the information for any of their own customers. Customers may retrieve the information for themselves.<br><br>
 
 Returns a L<CDN::Edgecast::Client::auto::Reporting::Element::DirStatsGetResponse|CDN::Edgecast::Client::auto::Reporting::Element::DirStatsGetResponse> object.
 
@@ -333,7 +411,7 @@ Returns a L<CDN::Edgecast::Client::auto::Reporting::Element::DirStatsGetResponse
 
 =head3 CompleteDownloadsGet
 
-FOR CUSTOMERS WITH ADVANCED REPORTS. <br>This method call will get complete downloads and download attempts for files.<br>Files included in this report are those greater than 50 kb.<br>Begin and end dates (inclusive) must be in UTC/GMT and data is granular to the day. So if you want to retrieve data for 2009-01-01, begin date would be '2009-01-01T00:00:00-00:00' and end date would be '2009-01-01T23:59:00-00:00'.<br>Media Type: 3=HTTP Large Object. HTTP Small Object, Windows and Flash are not available at this time.<br>Partners may retrieve the information for any of their own customers. Customers may retrieve the information for themselves.<br><br>NOTES:<br />* In calculating whether a hit is a "complete download," we consider status 200 hits and take into account byte-range requests where multiple hits can be part of one download.We also verify whether or not the data transferred is equal to or greater than the filesize in order to count the hit as a "complete download."<br />* This report is very interpretive by nature, and we have identified a few points that may alter the consistency and accuracy of this report.<br />(1) Different user-agents exhibit different behaviors. Files may show greater than 100% because traffic pattern cannot be well-captured based on user-agent behavior. <br />(2) Customer hits using FLV seek to jump around the contents of a file may not be represented correctly in this report.<br />(3) The downloads data for compressed files may be inaccurate because the recorded filesize is larger than the total bytes transferred. This would apply to customers that have compression enabled.
+FOR CUSTOMERS WITH ADVANCED REPORTS. <br>This method will get complete downloads and download attempts for files.<br>Files included in this report are those greater than 50 kb.<br>Begin and end dates (inclusive) must be in UTC/GMT and data is granular to the day. So if you want to retrieve data for 2009-01-01, begin date would be '2009-01-01T00:00:00-00:00' and end date would be '2009-01-01T23:59:00-00:00'.<br>Media Type: 3=HTTP Large Object. HTTP Small Object, Windows and Flash are not available at this time.<br>Partners may retrieve the information for any of their own customers. Customers may retrieve the information for themselves.<br><br>NOTES:<br />* In calculating whether a hit is a "complete download," we consider status 200 hits and take into account byte-range requests where multiple hits can be part of one download.We also verify whether or not the data transferred is equal to or greater than the filesize in order to count the hit as a "complete download."<br />* This report is very interpretive by nature, and we have identified a few points that may alter the consistency and accuracy of this report.<br />(1) Different user-agents exhibit different behaviors. Files may show greater than 100% because traffic pattern cannot be well-captured based on user-agent behavior. <br />(2) Customer hits using FLV seek to jump around the contents of a file may not be represented correctly in this report.<br />(3) The downloads data for compressed files may be inaccurate because the recorded filesize is larger than the total bytes transferred. This would apply to customers that have compression enabled.
 
 Returns a L<CDN::Edgecast::Client::auto::Reporting::Element::CompleteDownloadsGetResponse|CDN::Edgecast::Client::auto::Reporting::Element::CompleteDownloadsGetResponse> object.
 
@@ -348,7 +426,7 @@ Returns a L<CDN::Edgecast::Client::auto::Reporting::Element::CompleteDownloadsGe
 
 =head3 CnameStatsGet
 
-This method call will get bytes and hits by CNAME.<br>Begin and end dates are inclusive and data is granular to the hour. So if you want to retrieve data for 2009-01-01, begin date would be '2009-01-01T00:00:00-00:00' and end date would be '2009-01-01T23:59:00-00:00'.<br>Media Type: 3=HTTP Large Object, 8=HTTP Small Object<br>Partners may retrieve the information for any of their own customers. Customers may retrieve the information for themselves.<br><br>
+This method will get bytes and hits by CNAME.<br>Begin and end dates are inclusive and data is granular to the hour. So if you want to retrieve data for 2009-01-01, begin date would be '2009-01-01T00:00:00-00:00' and end date would be '2009-01-01T23:59:00-00:00'.<br>Media Type: 3=HTTP Large Object, 8=HTTP Small Object<br>Partners may retrieve the information for any of their own customers. Customers may retrieve the information for themselves.<br><br>
 
 Returns a L<CDN::Edgecast::Client::auto::Reporting::Element::CnameStatsGetResponse|CDN::Edgecast::Client::auto::Reporting::Element::CnameStatsGetResponse> object.
 
@@ -363,7 +441,7 @@ Returns a L<CDN::Edgecast::Client::auto::Reporting::Element::CnameStatsGetRespon
 
 =head3 CacheStatusStatsGet
 
-This method call will get hits by Cache Status.<br>Begin and end dates are inclusive and data is granular to the hour. So if you want to retrieve data for 2009-01-01, begin date would be '2009-01-01T00:00:00-00:00' and end date would be '2009-01-01T23:59:00-00:00'.<br>Media Type: 3=HTTP Large Object, 8=HTTP Small Object<br>Partners may retrieve the information for any of their own customers. Customers may retrieve the information for themselves.<br><br>
+This method will get hits by Cache Status.<br>Begin and end dates are inclusive and data is granular to the hour. So if you want to retrieve data for 2009-01-01, begin date would be '2009-01-01T00:00:00-00:00' and end date would be '2009-01-01T23:59:00-00:00'.<br>Media Type: 3=HTTP Large Object, 8=HTTP Small Object<br>Partners may retrieve the information for any of their own customers. Customers may retrieve the information for themselves.<br><br>
 
 Returns a L<CDN::Edgecast::Client::auto::Reporting::Element::CacheStatusStatsGetResponse|CDN::Edgecast::Client::auto::Reporting::Element::CacheStatusStatsGetResponse> object.
 
@@ -376,10 +454,52 @@ Returns a L<CDN::Edgecast::Client::auto::Reporting::Element::CacheStatusStatsGet
   },,
  );
 
+=head3 TrafficUsageGet
+
+This method will get billing statistics for the month for a customer. Billing closes on the 3rd day of each month at midnight GMT time.<br>BeginDate: Must be the first of a month. All billing stats are in UTC/GMT. <br>MediaType: 1=WMS, 2=FMS, 3=HTTP Large, 4=HTTPS Large, 8=HTTP Small, 9=HTTPS Small, 14=ADN, 15=ADN SSL <br>Region: 1=Global Standard, 2=North America & Europe, 3=Asia Pacific <br>UsageUnits: 1=Mbps, 2=GB <br>Partners may retrieve the information for any of their own customers. Customers may retrieve the information for themselves.<br><br>
+
+Returns a L<CDN::Edgecast::Client::auto::Reporting::Element::TrafficUsageGetResponse|CDN::Edgecast::Client::auto::Reporting::Element::TrafficUsageGetResponse> object.
+
+ $response = $interface->TrafficUsageGet( {
+    strCredential =>  $some_value, # string
+    strCustomerId =>  $some_value, # string
+    datBeginDate =>  $some_value, # dateTime
+    intMediaType =>  $some_value, # int
+    intRegion =>  $some_value, # int
+    intUsageUnits =>  $some_value, # int
+  },,
+ );
+
+=head3 StorageUsageGetMax
+
+This method will get the maximum storage usage for a customer during the time frame. Units are in GB.<br>Usage samples are taken throughout the day, so the number reflected is the maximum out of the samples.<br>Begin and end dates are inclusive.<br>Partners may retrieve the information for any of their own customers. Customers may retrieve the information for themselves.<br><br>
+
+Returns a L<CDN::Edgecast::Client::auto::Reporting::Element::StorageUsageGetMaxResponse|CDN::Edgecast::Client::auto::Reporting::Element::StorageUsageGetMaxResponse> object.
+
+ $response = $interface->StorageUsageGetMax( {
+    strCredential =>  $some_value, # string
+    strCustomerId =>  $some_value, # string
+    datBeginDate =>  $some_value, # dateTime
+    datEndDate =>  $some_value, # dateTime
+  },,
+ );
+
+=head3 StorageUsageGetLatest
+
+This method will get the latest sampled storage usage for a customer. Units are in GB.<br>Usage samples are taken throughout the day, so the number reflected is the latest sample.<br>Partners may retrieve the information for any of their own customers. Customers may retrieve the information for themselves.<br><br>
+
+Returns a L<CDN::Edgecast::Client::auto::Reporting::Element::StorageUsageGetLatestResponse|CDN::Edgecast::Client::auto::Reporting::Element::StorageUsageGetLatestResponse> object.
+
+ $response = $interface->StorageUsageGetLatest( {
+    strCredential =>  $some_value, # string
+    strCustomerId =>  $some_value, # string
+  },,
+ );
+
 
 
 =head1 AUTHOR
 
-Generated by SOAP::WSDL on Tue Sep 21 17:24:37 2010
+Generated by SOAP::WSDL on Wed Dec 15 10:58:29 2010
 
 =cut
